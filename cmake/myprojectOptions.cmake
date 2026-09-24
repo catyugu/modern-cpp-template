0b51236c0961f23ebfad2eda34e1f8d44f0d86fe
catyugu <141992786+catyugu@users.noreply.github.com>
2026-09-24 11:08:20 +0800

    fix: setup CMAKE_CXX_COMPILER_FRONTEND_VARIANT  manually so that it can work under cmake < 3.30

--- cmake/myprojectOptions.cmake
+++ cmake/myprojectOptions.cmake
@@ -1,7 +1,14 @@
 add_library(myproject_options INTERFACE)
 
-set(_MYPROJECT_GNU_FRONTEND $<CXX_COMPILER_FRONTEND_VARIANT:GNU>)
-set(_MYPROJECT_MSVC_FRONTEND $<CXX_COMPILER_FRONTEND_VARIANT:MSVC>)
+# 前端在配置期就已知，两个变量取常量 0/1，下面继续当条件用。
+# 不用 $<CXX_COMPILER_FRONTEND_VARIANT:...>：那个要 CMake 3.30，会让声明支持的 3.26~3.29 配置失败
+if(CMAKE_CXX_COMPILER_FRONTEND_VARIANT STREQUAL "MSVC")
+    set(_MYPROJECT_MSVC_FRONTEND 1)
+    set(_MYPROJECT_GNU_FRONTEND 0)
+else()
+    set(_MYPROJECT_MSVC_FRONTEND 0)
+    set(_MYPROJECT_GNU_FRONTEND 1)
+endif()
 
 target_compile_options(myproject_options INTERFACE
     $<${_MYPROJECT_GNU_FRONTEND}:
@@ -17,11 +24,9 @@ target_compile_options(myproject_options INTERFACE
     /utf-8
     /bigobj
     /Zc:__cplusplus
-    # 只在 /W4 + /WX 组合下才会把正常代码变成错误的、对库代码没有可行动信号的两条：
-    # 导出类继承/内嵌标准库类型（std::string、std::runtime_error 跨 DLL 使用）是正常做法，
-    # MSVC 文档也说明这类情况可以忽略，gtest（-wd4251 -wd4275）、re2、spdlog 同样关掉。
-    # 有意义的警告一律保留：4127（常量条件表达式）、4189/4100（未使用变量/参数）、4702（不可达
-    # 代码）等照常报错，配 /WX 时仍会中断构建（已实测）
+    # 只在 /W4 + /WX 下把正常代码变成错误的、对库代码没有可行动信号的两条：导出类继承/内嵌标准库
+    # 类型是正常做法，MSVC 文档也说明可忽略，gtest（-wd4251 -wd4275）、re2、spdlog 同样关掉。
+    # 4127、4189/4100、4702 等有意义的警告照常报错，配 /WX 时仍会中断构建（已实测）
     /wd4251 # 'type' needs to have dll-interface to be used by clients of class
     /wd4275 # non dll-interface class used as base for dll-interface class
     >
